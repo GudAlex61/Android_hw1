47d5c66070d189bc97a255f08a011ec54ea9e557
alexgud <aleksejgudis623@gmail.com>
2025-11-03 16:18:07 +0300

    done

---
+++ app/src/main/java/com/example/myapplication/CardsAdapter.kt
@@ -0,0 +1,63 @@
+package com.example.myapplication
+
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+
+class CardsAdapter : RecyclerView.Adapter<CardsAdapter.ViewHolder>() {
+
+    private val items = mutableListOf<Int>()
+
+    fun updateItems(newItems: List<Int>) {
+        items.clear()
+        items.addAll(newItems)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_card, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
+
+        // Исправляем установку квадратной формы
+        holder.itemView.post {
+            val width = holder.itemView.width
+            if (width > 0) {
+                val layoutParams = holder.itemView.layoutParams
+                layoutParams.height = width
+                holder.itemView.layoutParams = layoutParams
+            }
+        }
+    }
+
+    override fun getItemCount() = items.size
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val cardView: CardView = itemView.findViewById(R.id.cardView)
+        private val numberText: TextView = itemView.findViewById(R.id.numberText)
+
+        fun bind(number: Int) {
+            // Убеждаемся, что текст устанавливается и виден
+            numberText.text = number.toString()
+            numberText.setTextColor(ContextCompat.getColor(itemView.context, android.R.color.white))
+
+            val colorRes = if (number % 2 == 0) {
+                R.color.color_even
+            } else {
+                R.color.color_odd
+            }
+
+            cardView.setCardBackgroundColor(
+                ContextCompat.getColor(itemView.context, colorRes)
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.content.res.Configuration
+import android.view.LayoutInflater
+import android.view.View
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: CardsAdapter
+    private lateinit var fab: FloatingActionButton
+
+    private companion object {
+        const val ITEMS_KEY = "items_key"
+    }
+
+    private val items = mutableListOf<Int>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        fab = findViewById(R.id.fab)
+
+        savedInstanceState?.getIntArray(ITEMS_KEY)?.let { savedItems ->
+            items.clear()
+            items.addAll(savedItems.toList())
+        }
+
+        setupRecyclerView()
+        setupClickListeners()
+    }
+
+    private fun setupRecyclerView() {
+        adapter = CardsAdapter()
+
+        val spanCount = getColumnCount()
+        val layoutManager = GridLayoutManager(this, spanCount)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = adapter
+
+        adapter.updateItems(items)
+    }
+
+    private fun getColumnCount(): Int {
+        return if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            resources.getInteger(R.integer.landscape_columns)
+        } else {
+            resources.getInteger(R.integer.portrait_columns)
+        }
+    }
+
+    private fun setupClickListeners() {
+        fab.setOnClickListener {
+            addNewItem()
+        }
+    }
+
+    private fun addNewItem() {
+        items.add(items.size)
+        adapter.updateItems(items)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putIntArray(ITEMS_KEY, items.toIntArray())
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        // Полностью пересоздаём RecyclerView при смене ориентации
+        val spanCount = getColumnCount()
+        val layoutManager = GridLayoutManager(this, spanCount)
+        recyclerView.layoutManager = layoutManager
+
+        // Принудительно обновляем все элементы
+        adapter.notifyDataSetChanged()
+    }
+}
